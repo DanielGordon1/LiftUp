51d14e9bce30e2f341cae4aadab0b27625fa3dd3
Daniel Gordon <d_gordon1@icloud.com>
2017-11-21 11:44:32 +0100

    Make lift editable.

--- app/controllers/lifts_controller.rb
+++ app/controllers/lifts_controller.rb
@@ -1,14 +1,16 @@
 class LiftsController < ApplicationController
-  def index
-    @lifts = Lift.all
-  end
   def create
     @lift = Lift.new(lift_params)
+
     if @lift.save
       render json: @lift
     else
       render json: @lift.errors, status: :unprocessable_entity
     end
+  end
+
+  def index
+    @lifts = Lift.all
   end
 
   def destroy
@@ -17,8 +19,16 @@ class LiftsController < ApplicationController
     head :no_content
   end
 
-  private
+  def update
+    @lift = Lift.find(params[:id])
+    if @lift.update(lift_params)
+      render json: @lift
+    else
+      render json: @lift.errors, status: :unprocessable_entity
+    end
+  end
 
+  private
   def lift_params
     params.require(:lift).permit(:date, :liftname, :ismetric, :weightlifted, :repsperformed, :onerm)
   end
